04cde967e9b56eb04b4422ba413a5c37006ec717
Alexander Banks <alexbanks097@gmail.com>
2021-02-05 15:14:15 +0100

    fix client print

--- src/client.rs
+++ src/client.rs
@@ -3,6 +3,9 @@ use std::net::TcpStream;
 
 pub fn run(dest: &String) -> std::io::Result<()> {
     let mut stream = TcpStream::connect(dest)?;
+    println!("Starting client...\n  from: {}\n  to: {}", 
+        stream.local_addr().unwrap(), 
+        stream.peer_addr().unwrap());
 
     const DATA:&[u8] = b"Hello, World";
 
