9e70aa70a16eb9a55601d3006d593fecdb866fbb
Alexander Banks <alexbanks097@gmail.com>
2021-02-08 15:10:55 +0100

    improve challenge-response setup

--- src/client.rs
+++ src/client.rs
@@ -1,16 +1,40 @@
 use std::io::prelude::*;
-use std::net::TcpStream;
+use std::net::{TcpListener, TcpStream};
 
-pub fn run(dest: &String) -> std::io::Result<()> {
-    let mut stream = TcpStream::connect(dest)?;
+pub fn run() -> std::io::Result<()> {
+    println!("Trying to connect to {}...", crate::PROVER_ADDRESS);
+    let mut stream = TcpStream::connect(crate::PROVER_ADDRESS)?;
     println!("Starting client...\n  from: {}\n  to: {}", 
         stream.local_addr().unwrap(), 
         stream.peer_addr().unwrap());
 
-    const DATA:&[u8] = b"Hello, World";
+    const ATTEST_FLAG:&str = "attest";
+    
+    let data = format!("{};{}", &ATTEST_FLAG, crate::CLIENT_ADDRESS);
 
-    println!("Sending: {:?}...", DATA);
-    stream.write(DATA)?;
+    println!("Sending: {:?} to {:?}...", data, crate::PROVER_ADDRESS);
+    stream.write(data.as_bytes())?;
+    stream.shutdown(std::net::Shutdown::Both)?;
+
+    let listener = TcpListener::bind(crate::CLIENT_ADDRESS)?;
+    
+    println!("Awiting reply on {:?} from {:?}...", crate::CLIENT_ADDRESS, crate::PROVER_ADDRESS);
+    // handle response
+    for stream in listener.incoming() {
+        handle_response(stream?);
+        break;
+    }
 
     Ok(())
+}
+
+fn handle_response(stream:TcpStream) {
+    println!("Handling stream from: {}", stream.peer_addr().unwrap());
+    let mut stream = stream.try_clone().unwrap();
+    let mut challenge = String::new();
+
+    match stream.read_to_string(&mut challenge) {
+        Ok(_) => println!("Received: {:?}", challenge),
+        Err(e) => println!("Error: {}", e)
+    }
 }
--- src/server.rs
+++ src/server.rs
@@ -1,21 +1,36 @@
 use std::net::{TcpListener, TcpStream};
-use std::io::Read;
+use std::io::prelude::*;
 
 fn handle_client(stream: TcpStream) {
     println!("Handling stream from: {}", stream.peer_addr().unwrap());
 
     let mut stream = stream.try_clone().unwrap();
-    let mut buf = String::new();
+    let mut challenge = String::new();
 
-    match stream.read_to_string(&mut buf) {
-        Ok(_) => println!("Received: {:?}", buf),
+    match stream.read_to_string(&mut challenge) {
+        Ok(_) => println!("Received: {:?}", challenge),
         Err(e) => println!("Error: {}", e)
+    }
+
+
+    let mut challenge = challenge.split(";");
+    let _ = challenge.next().unwrap();
+    let client = challenge.next().unwrap();
+
+    println!("Trying to connect to client at {}", client);
+    if let Ok(mut client_stream) = TcpStream::connect(client) {
+        println!("Sending attestation reply...");
+        client_stream.write(b"Attestation Succesful").unwrap();
+        client_stream.shutdown(std::net::Shutdown::Both).unwrap();
+    }
+    else {
+        println!("Failed to connect...");
     }
 }
 
-pub fn run(addr: &String) -> std::io::Result<()> {
-    println!("Staring server at {}", addr);
-    let listener = TcpListener::bind(addr)?;
+pub fn run() -> std::io::Result<()> {
+    println!("Staring server at {}", crate::PROVER_ADDRESS);
+    let listener = TcpListener::bind(crate::PROVER_ADDRESS)?;
 
     // accept connections and process them serially
     for stream in listener.incoming() {
