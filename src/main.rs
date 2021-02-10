dc9815dc78496d216c2d4fcda29393f873deb26b
Alexander Banks <alexbanks097@gmail.com>
2021-02-10 08:39:19 +0100

    better naming convention

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,14 @@
 pub const PROVER_ADDRESS:&str = "127.0.0.1:8080";
 pub const CLIENT_ADDRESS:&str = "127.0.0.1:8181";
 
-mod client;
-mod server;
-
+mod verifier;
+mod prover;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     match args.get(1).unwrap_or(&String::new()).as_ref() {
-        "client" => client::run().unwrap(),
-        "server" => server::run().unwrap(),
+        "verifier" => verifier::run().unwrap(),
+        "prover" => prover::run().unwrap(),
         _ => println!("Usage: client | server [ip:port]")
     }
 }
